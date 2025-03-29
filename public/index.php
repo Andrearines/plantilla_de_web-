0bd3b3e4609e2ec934000d8212e7bf11962f4780
Steven Andre <andremartines2010@gmail.com>
2025-03-29 10:33:48 -0600

    MVC completo

--- public/index.php
+++ public/index.php
@@ -1,10 +1,7 @@
-<?php 
-require "../includes/app.php";
-template("header");
-?>
-<body>
-    
-<?php template("footer"); ?>
-<script src=""></script>
-</body>
-</html>
+<?php
+require_once __DIR__. '/../includes/app.php';
+use MVC\Router;
+use controllers\paginas;
+$r=new Router;
+$R->get("/",[paginas::class,'index']);
+$r->Rutas();
